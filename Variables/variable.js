b0c6f2ac822ecb38689ed3d2d196bc3ff151837d
MonowarHusain <MSImhaiKAR@2>
2025-12-05 03:04:36 +0600

    const and var

--- Variables/variable.js
+++ Variables/variable.js
@@ -22,3 +22,37 @@ console.log(boolean);
 console.log(undef);
 console.log(empty);
 console.log(number); 
+
+
+//more variables
+
+//const
+// we use const to define constant variable
+//we can not change the value of constant variable
+// we use const for defining variables whose value will not change throughout the program
+// we can use const to store important values like pi, gravity etc. or security keys, API keys etc.
+const a=1
+
+console.log(a)
+
+
+
+const m=1098
+//m=2 //error: Assignment to constant variable.
+console.log(m)
+
+
+
+//var
+//var is function scoped
+//we can reassign and redeclare var variable
+var x=10
+console.log(x)
+
+var x=20
+console.log(x)
+
+x=30
+console.log(x)      
+
+
